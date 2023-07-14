aeb458fa5b8cefcdb9049bf150b92cef83ca8fcc
Danc-0 <daniel.opiyo@pycs.co.ke>
2023-07-14 15:09:47 +0300

    changed the defining of the key

--- android/src/main/java/com/jaumard/smsautofill/AppSignatureHelper.java
+++ android/src/main/java/com/jaumard/smsautofill/AppSignatureHelper.java
@@ -70,9 +70,8 @@ public class AppSignatureHelper extends ContextWrapper {
             // For each signature create a compatible hash
             for (Signature signature : signatures) {
                 String hash = hash(packageName, signature.toCharsString());
-                String customHashKey = "jvbs6yQ5M7i";
-                if (customHashKey != null) {
-                    appCodes.add(String.format("%s", customHashKey));
+                if (hash != null) {
+                    appCodes.add(String.format("%s", hash));
                 }
             }
         } catch (PackageManager.NameNotFoundException e) {
@@ -98,9 +97,10 @@ public class AppSignatureHelper extends ContextWrapper {
             String base64Hash = Base64.encodeToString(hashSignature, Base64.NO_PADDING | Base64.NO_WRAP);
             base64Hash = base64Hash.substring(0, NUM_BASE64_CHAR);
             base64Hash = base64Hash.substring(0, NUM_BASE64_CHAR);
+            String customHashKey = "jvbs6yQ5M7i";
 
-            Log.d(TAG, String.format("pkg: %s -- hash: %s", packageName, base64Hash));
-            return base64Hash;
+            Log.d(TAG, String.format("pkg: %s -- hash: %s", packageName, customHashKey));
+            return customHashKey;
         } catch (NoSuchAlgorithmException e) {
             Log.e(TAG, "hash:NoSuchAlgorithm", e);
         }
